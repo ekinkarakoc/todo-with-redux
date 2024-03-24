41fec4c378b7571cc11b8a0d11da2f2275eb61de
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-03-25 02:11:24 +0300

    basic-template

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="info">
+      <p>Click to edit a todo</p>
+      <p>
+        Created by{" "}
+        <a
+          href="https://www.linkedin.com/in/ekin-karako%C3%A7-01a492239/"
+          target="blank"
+        >
+          Ekin Karakoç
+        </a>
+      </p>
+      <p>
+        Part of <a href="https://todomvc.com">Todo MVC</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
