053ca47ca11c36a67c89f6f94286d2c3fdc922ea
ekinkarakoc <ekinkarakoc66@gmail.com>
2024-03-25 11:29:40 +0300

    addTodo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -7,17 +7,9 @@ function TodoList() {
   return (
     <div>
       <ul className="todo-list">
-        {/* <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn JavaScript</label>
-            <button className="destroy"></button>
-          </div>
-        </li> */}
-
         {items.map((item) => {
           return (
-            <li key={item.id}>
+            <li key={item.id} className={item.completed ? "completed" : ""}>
               <div className="view">
                 <input className="toggle" type="checkbox" />
                 <label>{item.title}</label>
